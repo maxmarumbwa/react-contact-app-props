76e5e35785f8105b343ae8e82fe385c47ef388df
maxmarumbwa <maxmarumbwa@gmail.com>
2025-11-11 10:05:14 +0200

    add contacts from json

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import contacts from "./contacts";
 
 function Card() {
         return (
@@ -7,15 +8,15 @@ function Card() {
                         <div>
                                 <div className="card">
                                         <div className="top">
-                                                <h2>Beyonce</h2>
+                                                <h2>{contacts[0].name}</h2>
                                                 <img
-                                                        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+                                                        src={contacts[0].imgURL}
                                                         alt="avatar_img"
                                                 />
                                         </div>
                                         <div className="bottom">
-                                                <p>+123 456 789</p>
-                                                <p>b@beyonce.com</p>
+                                                <p>{contacts[0].phone}</p>
+                                                <p>{contacts[0].email}</p>
                                         </div>
                                 </div>
                         </div>
@@ -23,30 +24,30 @@ function Card() {
                         <div>
                                 <div className="card">
                                         <div className="top">
-                                                <h2>Jack Bauer</h2>
+                                                <h2>{contacts[1].name}</h2>
                                                 <img
-                                                        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+                                                        src={contacts[1].imgURL}
                                                         alt="avatar_img"
                                                 />
                                         </div>
                                         <div className="bottom">
-                                                <p>+123 456 789</p>
-                                                <p>jack@nowhere.com</p>
+                                                <p>{contacts[1].phone}</p>
+                                                <p>{contacts[1].email}</p>
                                         </div>
                                 </div>
                         </div>
                         <div>
                                 <div className="card">
                                         <div className="top">
-                                                <h2>Chuck Norris</h2>
+                                                <h2>{contacts[2].name}</h2>
                                                 <img
-                                                        src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+                                                        src={contacts[2].imgURL}
                                                         alt="avatar_img"
                                                 />
                                         </div>
                                         <div className="bottom">
-                                                <p>+123 456 789</p>
-                                                <p>gmail@chucknorris.com</p>
+                                                <p>{contacts[2].phone}</p>
+                                                <p>{contacts[2].email}</p>
                                         </div>
                                 </div>
                         </div>
